19ff8d7896ffd357b4636ac7904341203efa0b1f
Adrian Irving-Beer <wisq@wisq.net>
2022-03-05 01:23:09 -0500

    `mix new ff14_calendar`

---
+++ lib/ff14_calendar.ex
@@ -0,0 +1,18 @@
+defmodule Ff14Calendar do
+  @moduledoc """
+  Documentation for `Ff14Calendar`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ff14Calendar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ff14_calendar_test.exs
@@ -0,0 +1,8 @@
+defmodule Ff14CalendarTest do
+  use ExUnit.Case
+  doctest Ff14Calendar
+
+  test "greets the world" do
+    assert Ff14Calendar.hello() == :world
+  end
+end
