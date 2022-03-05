a47b6c62d47ccb9fb69b01bca39773806502d6db
Adrian Irving-Beer <wisq@wisq.net>
2022-03-05 07:59:52 -0500

    Add logging.

--- lib/mix/task/ff14.put.ex
+++ lib/mix/task/ff14.put.ex
@@ -1,9 +1,12 @@
 defmodule Mix.Tasks.Ff14.Put do
   use Mix.Task
+  require Logger
 
   @moduledoc false
 
   def run([url]) do
+    Logger.configure_backend(:console, Application.get_env(:logger, :console))
+
     ensure_env("AWS_ACCESS_KEY_ID")
     ensure_env("AWS_SECRET_ACCESS_KEY")
 
@@ -15,14 +18,22 @@ defmodule Mix.Tasks.Ff14.Put do
     |> s3_put(url)
   end
 
+  def run(_) do
+    Mix.raise("Usage: mix ff14.put s3://bucket/path")
+  end
+
   defp s3_put(ics, url) do
     {bucket, path} = parse_url(url)
 
     ExAws.S3.put_object(bucket, path, ics)
     |> ExAws.request!()
 
+    Logger.info("Uploaded #{byte_size(ics)} bytes to #{url}")
+
     ExAws.S3.put_object_acl(bucket, path, acl: :public_read)
     |> ExAws.request!()
+
+    Logger.info("Set ACL for #{url} to public read-only.")
   end
 
   defp parse_url(url) do
@@ -42,7 +53,7 @@ defmodule Mix.Tasks.Ff14.Put do
   defp ensure_env(var) do
     case System.fetch_env(var) do
       {:ok, _} -> :ok
-      :error -> raise "Environment variable #{var} must be set"
+      :error -> Mix.raise("Environment variable #{var} must be set")
     end
   end
 end
