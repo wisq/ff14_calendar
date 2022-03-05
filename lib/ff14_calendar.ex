44f44fe88d889791791ed19f9b53bded756527ff
Adrian Irving-Beer <wisq@wisq.net>
2022-03-05 05:07:12 -0500

    Remove default code & test.

--- lib/ff14_calendar.ex
+++ lib/ff14_calendar.ex
@@ -1,18 +1,5 @@
-defmodule Ff14Calendar do
+defmodule FF14Calendar do
   @moduledoc """
-  Documentation for `Ff14Calendar`.
+  Documentation for `FF14Calendar`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ff14Calendar.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ff14_calendar_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Ff14CalendarTest do
-  use ExUnit.Case
-  doctest Ff14Calendar
-
-  test "greets the world" do
-    assert Ff14Calendar.hello() == :world
-  end
-end
